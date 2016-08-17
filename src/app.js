7e2da862e028a18ae44be20d88695b4b0c65b847
jan-mikulka <jan.mikulka@email.cz>
2016-08-17 21:08:11 +0200

    add basic root element

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+var theme = getMuiTheme();
+
+ReactDOM.render(
+	<div>
+    <MuiThemeProvider muiTheme={theme}>
+      <div>
+				<View/>
+		  </div>
+    </MuiThemeProvider>
+  </div>,
+	document.getElementById('app')
+);
+
