bfa2c242aefb370278949b8217e8cd23204e99ec
jan-mikulka <jan.mikulka@email.cz>
2016-08-17 23:33:07 +0200

    steal and implement The Algorithm

--- src/components/View.js
+++ src/components/View.js
@@ -10,6 +10,83 @@ const initialState = {
 	userInput: ''
 }
 
+var CustomCounter = function(min, max) {
+    this.min = min.slice(0)
+    this.max = max.slice(0)
+    this.curr = this.min.slice(0)
+    this.length = this.min.length
+}
+
+CustomCounter.prototype.increment = function() {
+    for (var i = this.length - 1, ii = 0; i >= ii; i--) {
+        this.curr[i] += 1
+        if (this.curr[i] > this.max[i]) {
+            this.curr[i] = 0
+        } else {
+            break
+        }
+    }
+}
+
+CustomCounter.prototype.is_max = function() {
+    for (var i = 0, ii = this.length; i < ii; ++i) {
+        if (this.curr[i] !== this.max[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+var PhoneNumber = function(phone_number) {
+    this.phone_number = phone_number
+    this.combinations = []
+}
+
+PhoneNumber.number_to_combinations = keyboard;
+
+PhoneNumber.prototype.get_combination_by_digit = function(digit) {
+    return PhoneNumber.number_to_combinations[digit]
+}
+
+PhoneNumber.prototype.add_combination_by_indexes = function(indexes) {
+    var combination = ''
+    for (var i = 0, ii = indexes.length; i < ii; ++i) {
+        var phone_number_digit = this.phone_number[i]
+        combination += this.get_combination_by_digit(phone_number_digit)[indexes[i]]
+    }
+
+    this.combinations.push(combination)
+}
+
+PhoneNumber.prototype.update_combinations = function() {
+    var min_indexes = []
+      , max_indexes = []
+
+    for (var i = 0, ii = this.phone_number.length; i < ii; ++i) {
+        var digit = this.phone_number[i]
+        min_indexes.push(0)
+        max_indexes.push(this.get_combination_by_digit(digit).length - 1)
+    }
+
+    var c = new CustomCounter(min_indexes, max_indexes)
+
+    while(true) {
+        this.add_combination_by_indexes(c.curr)
+        c.increment()
+
+        if (c.is_max()) {
+            this.add_combination_by_indexes(c.curr)
+            break
+        }
+    }
+}
+
+function t9(userInput) {
+	var phone_number = new PhoneNumber(userInput)
+	phone_number.update_combinations()
+	console.log(phone_number.combinations)
+}
+
 export default class View extends React.Component {
 
   constructor() {
