0609d893a0c191c3ef68004404a54584bfda022c
jan-mikulka <jan.mikulka@email.cz>
2016-08-18 00:11:55 +0200

    move styles to separate file

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+export default {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 700,
+    overflowY: 'auto',
+    marginBottom: 24,
+  },
+  chip: {
+  	margin: 4
+  }
+};
